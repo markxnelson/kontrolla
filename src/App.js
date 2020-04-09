24ea6cefe58956145cc363b237ba58694d43abff
Mark Nelson <mark.x.nelson@oracle.com>
2020-04-09 19:50:21 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming soon!
+        </p>
+          <i>Kontrolled Kaos!</i>
+      </header>
+    </div>
+  );
+}
+
+export default App;
